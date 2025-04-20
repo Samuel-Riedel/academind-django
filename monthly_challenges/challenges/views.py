544a4a6bc06fc6cc509ebf9f80074416a0a8af01
Samuel_Riedel <samuel.riedel@outlook.com>
2025-04-20 17:38:32 +0200

    fix:_add_request_parameter_to_index_view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -27,6 +27,16 @@ perritos_challenge = {
 
 
 # Create your views here.
+def index(request):
+    list_items =""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request,month):
     months = list(monthly_challenges.keys())
@@ -43,7 +53,7 @@ def monthly_challenge(request, month):
         response_data = f"<h1>{challenge_text}</h1>"
         return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound(f"<h1>This month is not supported</h1>")
+        return HttpResponseNotFound(f"<h1>This month is not supported!</h1>")
     
 
 
